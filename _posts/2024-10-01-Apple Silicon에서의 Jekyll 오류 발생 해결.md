df22f6244d3d56bd827465c8dc7236d5b059f646
HWIJAE KIM <dusan960766@gmail.com>
2024-10-13 23:30:55 +0900

    dev_home layout change

--- _posts/2024-10-01-Apple Silicon에서의 Jekyll 오류 발생 해결.md
+++ _posts/2024-10-01-Apple Silicon에서의 Jekyll 오류 발생 해결.md
@@ -1,7 +1,7 @@
 ---
 title:  "Apple Silicon에서의 Jekyll 오류 발생 해결"
 header:
-  teaser: "/assets/images/profile.jpg"
+  teaser: "/assets/images/posts_teaser/apple_silicon_jekyll.jpg"
 categories:
   - Jekyll
   - solve
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,7 +3,7 @@ var store = [{
         "excerpt":"Chat GPT 프롬프트 macOS 터미널을 이용해서 jekyll bundle을 설치하고 있는데 eventmachine 1.2.7 설치 도중 오류가 난대. 왜 이럴까? -2024.09.30 1. Xcode Command Line Tools 재설치 이미 명령줄 도구를 설치했더라도 일부 구성 요소가 손상되었을 수 있으니, 다시 설치하거나 업데이트를 시도해 보세요. sudo rm -rf /Library/Developer/CommandLineTools xcode-select --install 2. g++와 clang 설치...","categories": ["Jekyll","solve"],
         "tags": ["jekyll","macos","apple silicon","문제해결"],
         "url": "/jekyll/solve/Apple-Silicon%EC%97%90%EC%84%9C%EC%9D%98-Jekyll-%EC%98%A4%EB%A5%98-%EB%B0%9C%EC%83%9D-%ED%95%B4%EA%B2%B0/",
-        "teaser": "/assets/images/profile.jpg"
+        "teaser": "/assets/images/posts_teaser/apple_silicon_jekyll.jpg"
       },{
         "title": "안녕하세요",
         "excerpt":"Jekyll을 이용하여 Github Pages를 만들었습니다.   아직 시험단계로 보완할 부분이 많습니다.   키워드   성실한, 신뢰감 있는, 창의적인        이력서 폼(초안)   구글시트        Instagram: @hwijaekim_portfolio   Behance: HwijaeKim   LinkedIn: HwijaeKim   Github: HwijaeKim          —제작한 페이지—   No CSS Club MiniGame   청사진(공모전)   자기PR페이지(교내)   ","categories": ["Education"],
@@ -21,5 +21,5 @@ var store = [{
         "excerpt":"제목 내용 설명 테스트1 동해물과백두산이마르고닳도록 테스트3 First Header Second Header Third Header First row Data Very long data entry Second row Cell Cell Third row Cell that spans across two columns   날짜 내용 기관/결과 기여 구분 2025.02 계원예술대학교 디지털미디어디자인과 전문학사 졸업 예정   학력 2024.07 계원예술대학교 디지털미디어디자인과 2024-2학기 수석...","categories": ["연습"],
         "tags": ["markdown","table"],
         "url": "/%EC%97%B0%EC%8A%B5/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4%EC%97%90%EC%84%9C-%ED%91%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
-        "teaser": null
+        "teaser": "/assets/images/profile.jpg"
       }]
