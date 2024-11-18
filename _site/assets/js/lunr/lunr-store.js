37f9abc946f4d2238c1179df00e84195aad55137
HWIJAE KIM <dusan960766@gmail.com>
2024-11-18 15:06:17 +0900

    config update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,48 +2,48 @@ var store = [{
         "title": "아라비아 사막의 반도체 이야기",
         "excerpt":"계원예술대학교 1학년 2023-2학기 [콘텐츠디자인] 교과목 우수작                    ","categories": ["Portfolio","Design"],
         "tags": ["web design","figma","photoshop","illustrator","ui/ux"],
-        "url": "/portfolio/design/%EC%95%84%EB%9D%BC%EB%B9%84%EC%95%84%EC%82%AC%EB%A7%89%EC%9D%98-%EB%B0%98%EB%8F%84%EC%B2%B4-%EC%9D%B4%EC%95%BC%EA%B8%B0/",
-        "teaser": "/assets/images/posts_img/semiconductor/main.jpg"
+        "url": "/hwijaekim.github.io/portfolio/design/%EC%95%84%EB%9D%BC%EB%B9%84%EC%95%84%EC%82%AC%EB%A7%89%EC%9D%98-%EB%B0%98%EB%8F%84%EC%B2%B4-%EC%9D%B4%EC%95%BC%EA%B8%B0/",
+        "teaser": "/hwijaekim.github.io/assets/images/posts_img/semiconductor/main.jpg"
       },{
         "title": "macOS 콘셉트의 자기PR 페이지",
         "excerpt":"계원예술대학교 1학년 2023-2학기 [웹퍼블리싱] 교과목 우수작  ","categories": ["Portfolio","Code"],
         "tags": ["HTML/CSS/JS"],
-        "url": "/portfolio/code/macOS-%EC%BD%98%EC%85%89%ED%8A%B8%EC%9D%98-%EC%9E%90%EA%B8%B0PR-%ED%8E%98%EC%9D%B4%EC%A7%80/",
-        "teaser": "/assets/images/posts_img/pr/teaser.png"
+        "url": "/hwijaekim.github.io/portfolio/code/macOS-%EC%BD%98%EC%85%89%ED%8A%B8%EC%9D%98-%EC%9E%90%EA%B8%B0PR-%ED%8E%98%EC%9D%B4%EC%A7%80/",
+        "teaser": "/hwijaekim.github.io/assets/images/posts_img/pr/teaser.png"
       },{
         "title": "No CSS Club 미니게임",
         "excerpt":"HTML/CSS/Vanilla JS를 이용하여 제작한 웹 게임 Title Description 프로젝트명 JavaScript를 이용한 No CSS Club 미니게임 상세 전공과목 [자바스크립트프로그래밍] 프로젝트 시간 안에 영역 내 모든 CSS요소를 클리하여 제거하는 게임을 제작 기간 2024.05 ~ 2024.06 인원 개인 담당분야 게임 기획, 레이아웃 구성, CSS 스타일, 게임기능 구현 특이사항 CSS를 이용하여 Windows 98 스타일의...","categories": ["Portfolio","Code"],
         "tags": ["HTML/CSS/JS","game"],
-        "url": "/portfolio/code/No-CSS-Club-%EB%AF%B8%EB%8B%88%EA%B2%8C%EC%9E%84/",
-        "teaser": "/assets/images/posts_img/nocssclub/teaser.png"
+        "url": "/hwijaekim.github.io/portfolio/code/No-CSS-Club-%EB%AF%B8%EB%8B%88%EA%B2%8C%EC%9E%84/",
+        "teaser": "/hwijaekim.github.io/assets/images/posts_img/nocssclub/teaser.png"
       },{
         "title": "제 30회 커뮤니케이션디자인국제공모전 수상작 [청사진]",
         "excerpt":"Title Description 프로젝트명 청사진(BLUEPRINT) 목적 학교 밖 청소년들의 미래를 준비하기 위한 솔루션 제 30회 커뮤니케이션디자인국제공모전 일반/대학부 출품 기간 2024.03 ~ 2024.06 인원 기획1, 디자인1, 개발1 담당분야 팀장 및 서비스 비디오 촬영, 웹 콘텐츠 소스 제작 및 HTML/CSS/JS를 이용한 웹 제작 및 GitHub 배포 특이사항 HTML/CSS/JS 세 가지를 동시에 이용하여 처음으로...","categories": ["Portfolio","Code"],
         "tags": ["HTML/CSS/JS","공모전"],
-        "url": "/portfolio/code/%EC%A0%9C-30%ED%9A%8C-%EC%BB%A4%EB%AE%A4%EB%8B%88%EC%BC%80%EC%9D%B4%EC%85%98%EB%94%94%EC%9E%90%EC%9D%B8%EA%B5%AD%EC%A0%9C%EA%B3%B5%EB%AA%A8%EC%A0%84-%EC%88%98%EC%83%81%EC%9E%91-%EC%B2%AD%EC%82%AC%EC%A7%84/",
-        "teaser": "/assets/images/posts_img/blueprint/blueprint.png"
+        "url": "/hwijaekim.github.io/portfolio/code/%EC%A0%9C-30%ED%9A%8C-%EC%BB%A4%EB%AE%A4%EB%8B%88%EC%BC%80%EC%9D%B4%EC%85%98%EB%94%94%EC%9E%90%EC%9D%B8%EA%B5%AD%EC%A0%9C%EA%B3%B5%EB%AA%A8%EC%A0%84-%EC%88%98%EC%83%81%EC%9E%91-%EC%B2%AD%EC%82%AC%EC%A7%84/",
+        "teaser": "/hwijaekim.github.io/assets/images/posts_img/blueprint/blueprint.png"
       },{
         "title": "Apple Silicon에서의 Jekyll 오류 발생 해결",
         "excerpt":"Chat GPT 프롬프트 macOS 터미널을 이용해서 jekyll bundle을 설치하고 있는데 eventmachine 1.2.7 설치 도중 오류가 난대. 왜 이럴까? -2024.09.30 1. Xcode Command Line Tools 재설치 이미 명령줄 도구를 설치했더라도 일부 구성 요소가 손상되었을 수 있으니, 다시 설치하거나 업데이트를 시도해 보세요. sudo rm -rf /Library/Developer/CommandLineTools xcode-select --install 2. g++와 clang 설치...","categories": ["Research"],
         "tags": ["jekyll","macos","apple silicon","문제해결"],
-        "url": "/research/Apple-Silicon%EC%97%90%EC%84%9C%EC%9D%98-Jekyll-%EC%98%A4%EB%A5%98-%EB%B0%9C%EC%83%9D-%ED%95%B4%EA%B2%B0/",
-        "teaser": "/assets/images/posts_img/apple_silicon_jekyll/apple_silicon_jekyll.jpg"
+        "url": "/hwijaekim.github.io/research/Apple-Silicon%EC%97%90%EC%84%9C%EC%9D%98-Jekyll-%EC%98%A4%EB%A5%98-%EB%B0%9C%EC%83%9D-%ED%95%B4%EA%B2%B0/",
+        "teaser": "/hwijaekim.github.io/assets/images/posts_img/apple_silicon_jekyll/apple_silicon_jekyll.jpg"
       },{
         "title": "안녕하세요",
         "excerpt":"Jekyll을 이용하여 Github Pages를 만들었습니다.   아직 시험단계로 보완할 부분이 많습니다.   키워드   성실한, 신뢰감 있는, 창의적인        이력서 폼(초안)   구글시트        Instagram: @hwijaekim_portfolio   Behance: HwijaeKim   LinkedIn: HwijaeKim   Github: HwijaeKim          —제작한 페이지—   No CSS Club MiniGame   청사진(공모전)   자기PR페이지(교내)   ","categories": ["Research"],
         "tags": ["update"],
-        "url": "/research/%EC%95%88%EB%85%95%ED%95%98%EC%84%B8%EC%9A%94/",
-        "teaser": "/assets/images/profile.jpg"
+        "url": "/hwijaekim.github.io/research/%EC%95%88%EB%85%95%ED%95%98%EC%84%B8%EC%9A%94/",
+        "teaser": "/hwijaekim.github.io/assets/images/profile.jpg"
       },{
         "title": "마크다운에서 표 만들기",
         "excerpt":"제목 내용 설명 테스트1 동해물과백두산이마르고닳도록 테스트3 First Header Second Header Third Header First row Data Very long data entry Second row Cell Cell Third row Cell that spans across two columns   날짜 내용 기관/결과 기여 구분 2025.02 계원예술대학교 디지털미디어디자인과 전문학사 졸업 예정   학력 2024.07 계원예술대학교 디지털미디어디자인과 2024-2학기 수석...","categories": ["Research"],
         "tags": ["markdown","table"],
-        "url": "/research/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4%EC%97%90%EC%84%9C-%ED%91%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
-        "teaser": "/assets/images/profile.jpg"
+        "url": "/hwijaekim.github.io/research/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4%EC%97%90%EC%84%9C-%ED%91%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
+        "teaser": "/hwijaekim.github.io/assets/images/profile.jpg"
       },{
         "title": "계원예술대학교 디지털미디어디자인과 졸업작품 [잇토피아]",
         "excerpt":"               Title       Description                       프로젝트명       EATOPIA(잇토피아)                 상세       계원예술대학교 졸업작품 아이 맞춤형 XR글래스 콘텐츠를 활용한 어린이 식습관 개선 서비스                 기간       2024.09 ~ 2024.11                 인원       기획2, 디자인3, 개발1                 담당분야       실사 영상 촬영, 웹 사이트 구현                 특이사항       -                 관련 링크       -          ","categories": ["Portfolio","Code"],
         "tags": ["HTML/CSS/JS","졸업작품"],
-        "url": "/portfolio/code/%EA%B3%84%EC%9B%90%EC%98%88%EC%88%A0%EB%8C%80%ED%95%99%EA%B5%90-%EB%94%94%EC%A7%80%ED%84%B8%EB%AF%B8%EB%94%94%EC%96%B4%EB%94%94%EC%9E%90%EC%9D%B8%EA%B3%BC-%EC%A1%B8%EC%97%85%EC%9E%91%ED%92%88-%EC%9E%87%ED%86%A0%ED%94%BC%EC%95%84/",
-        "teaser": "/assets/images/posts_img/eatopia/eatopia_D.png"
+        "url": "/hwijaekim.github.io/portfolio/code/%EA%B3%84%EC%9B%90%EC%98%88%EC%88%A0%EB%8C%80%ED%95%99%EA%B5%90-%EB%94%94%EC%A7%80%ED%84%B8%EB%AF%B8%EB%94%94%EC%96%B4%EB%94%94%EC%9E%90%EC%9D%B8%EA%B3%BC-%EC%A1%B8%EC%97%85%EC%9E%91%ED%92%88-%EC%9E%87%ED%86%A0%ED%94%BC%EC%95%84/",
+        "teaser": "/hwijaekim.github.io/assets/images/posts_img/eatopia/eatopia_D.png"
       }]
